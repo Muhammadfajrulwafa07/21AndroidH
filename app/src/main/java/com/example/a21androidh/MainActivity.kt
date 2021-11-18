8822bf65eff2d6a70e69a1ad82898b93725ec0c6
Muhammadfajrulwafa07 <Muhammadfajrulwafatkj@gmail.com>
2021-11-18 16:00:48 +0800

    EditText, val dan var

--- app/src/main/java/com/example/a21androidh/MainActivity.kt
+++ app/src/main/java/com/example/a21androidh/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.a21androidh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val noHandphoneEditText = findViewById<EditText>(R.id.noHandphoneEditText)
+        val jarakEditText = findViewById<EditText>(R.id.jarakEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
+
+        simpanButton.setOnClickListener {
+
+            val nama = namaEditText.text.toString()
+            var jarak = jarakEditText.text.toString().toInt()
+            jarak = jarak * 1000
+
+            Toast.makeText(this,"Hai $jarak",Toast.LENGTH_SHORT).show()
+        }
+
+        batalButton.setOnClickListener {
+            finish()
+        }
     }
 }
